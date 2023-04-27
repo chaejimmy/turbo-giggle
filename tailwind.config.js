a21e3043c8e8e837cf94751323e4f4dea52eb67e
Jimmy <chaejimmy1@gmail.com>
2023-04-27 08:44:43 -0400

    totel

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/pages/**/*.{js,ts,jsx,tsx}',
+    './src/components/**/*.{js,ts,jsx,tsx}',
+    './src/app/**/*.{js,ts,jsx,tsx}',
+  ],
+  theme: {
+    extend: {
+      backgroundImage: {
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+        'gradient-conic':
+          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      },
+    },
+    colors: {
+      'purple': '#574EFA',
+      'white' : '#ffffff' ,
+      'blue'  :  '#574EFA',
+      'black' :  '#000000',
+      'grey':"#272d37"
+     }
+  },
+  plugins: [],
+}
